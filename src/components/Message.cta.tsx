fb0534645d2e04b00104fb715546d34703a11ef8
dapabe <dapadev@hotmail.com>
2024-01-25 20:55:08 -0300

    add: react-aria form

--- src/components/Message.cta.tsx
+++ src/components/Message.cta.tsx
@@ -1,8 +1,15 @@
 "use client";
 import { ChatBubbleLeftIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
-import { useZorm } from "react-zorm";
 import * as z from "zod";
 import { useFormspark } from "@formspark/use-formspark";
+import {
+	Button,
+	Form,
+	Label,
+	TextArea,
+	TextField,
+} from "react-aria-components";
+import { useZorm } from "react-zorm";
 
 const schema = z.object({
 	msg: z
@@ -13,6 +20,7 @@ const schema = z.object({
 			"No se admiten renglones vacios."
 		),
 });
+
 export const MessageCTA = () => {
 	const [submit] = useFormspark({
 		formId: process.env.NEXT_PUBLIC_FORMSPARK_ID!,
@@ -21,46 +29,44 @@ export const MessageCTA = () => {
 	const zo = useZorm("cta", schema, {
 		onValidSubmit: async (evt) => {
 			evt.preventDefault();
-			await submit({ message: evt.data.msg });
+			if (zo.validation?.success) submit(evt.data);
 		},
 	});
 
 	return (
-		<form ref={zo.ref} className="relative">
-			<label
-				htmlFor="htmlCTA"
-				className={`relative block rounded-md border shadow-sm ${
-					zo.errors.msg("border-red-600 focus-within:border-red-600") ??
-					"border-gray-400 focus-within:border-teal-400 focus-within:ring-teal-400"
-				}`}
-			>
-				<span className="pointer-events-none absolute start-2.5 -top-3 bg-white p-0.5 text-sm text-gray-700">
+		<Form ref={zo.ref}>
+			<TextField name={zo.fields.msg()} className={`form-control relative`}>
+				<Label
+					aria-describedby={zo.fields.msg("id")}
+					htmlFor={zo.fields.msg("id")}
+					className="absolute start-2.5 -top-3 p-0.5 text-sm text-neutral bg-white"
+				>
 					Contactame
 					<ChatBubbleLeftIcon className="w-6 ml-1 inline" />
-				</span>
-				<textarea
-					id="htmlCTA"
-					name={zo.fields.msg()}
-					className={`peer border-none bg-transparent py-4 px-2 resize-none w-full focus:border-transparent focus:outline-none focus:ring-0`}
-					placeholder="Hola Patricio, me comunico contigo para ... este es mi correo ejemplo@gmail.com"
+				</Label>
+				<TextArea
+					className={`textarea resize-none ${
+						zo.errors.msg("textarea-error") ?? "textarea-info"
+					}`}
+					placeholder="ej: Hola Patricio, me comunico contigo para ... este es mi correo ejemplo@gmail.com"
 				/>
-				<button
+
+				<div className="label relative">
+					{zo.errors.msg((e) => (
+						<span className="label-text-alt text-error text-sm absolute top-0.5">
+							{e.message}
+						</span>
+					))}
+				</div>
+				<Button
 					type="submit"
-					className={`bg-white p-2 rounded-full hover:cursor-pointer absolute -bottom-4 -right-4 transition-colors border ${
-						zo.errors.msg(
-							"border-red-600 hover:bg-white text-red-600 hover:text-red-600 peer-focus:border-red-600 "
-						) ??
-						"text-gray-400 border-gray-400 hover:text-white hover:bg-teal-400 active:bg-teal-600 peer-focus:border-teal-600"
+					className={`btn btn-circle absolute text-white bottom-0 -right-4 ${
+						zo.errors.msg("btn-error") ?? "btn-info"
 					}`}
 				>
 					<EnvelopeIcon className="w-6" />
-				</button>
-			</label>
-			{zo.errors.msg((e) => (
-				<span tabIndex={-1} className="text-red-600 text-sm absolute top-full">
-					{e.message}
-				</span>
-			))}
-		</form>
+				</Button>
+			</TextField>
+		</Form>
 	);
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+import type { Config as DaiConfig } from "daisyui";
 
-const config: Config = {
+const config: Config & { daisyui: DaiConfig } = {
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -10,6 +11,16 @@ const config: Config = {
 		fontFamily: {
 			comfortaa: ["Comfortaa", "Open Sans"],
 		},
+	},
+	plugins: [require("daisyui")],
+	daisyui: {
+		themes: [
+			{
+				redux: {
+					...require("daisyui/src/theming/themes")["fantasy"],
+				},
+			},
+		],
 	},
 };
 export default config;
